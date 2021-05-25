9c6783cf97a91d67c1e4c82aee167ee27a742fb6
TheTomik93 <TheTomik93@gmail.com>
2021-05-25 23:03:47 +0200

    Added basic UI, changed js code

--- logic.js
+++ logic.js
@@ -2,98 +2,73 @@ let playerPlayed;
 let computerPlayed;
 let playerScore = 0;
 let computerScore = 0;
+let gameOver = false;
+
+const choices = ["Rock", "Paper", "Scissors"];
+
+document.getElementById("Rock").addEventListener("click", game);
+document.getElementById("Paper").addEventListener("click", game);
+document.getElementById("Scissors").addEventListener("click", game);
+document.getElementById("newGame").addEventListener("click", newGame);
+
+function newGame(){
+    playerScore = 0;
+    computerScore = 0;
+    gameOver = false;
+    document.getElementById('score').innerHTML = `To play the game simply click Rock, Paper, or Scissors! Player score: ${playerScore} ... Computer score: ${computerScore}`;
+}
 
 function game(){
-    while(playerScore < 5 && computerScore < 5){
-        let userchoice = prompt("rock / paper / scissors").toLowerCase();
-        switch (userchoice){
-            case "rock":
-                rock();
-                break;
-            case "paper":
-                paper();
-                break;
-            case "scissors":
-                scissors();
-                break;
-            default:
-                alert("only rock / paper / scissors input accepted");
+    if (gameOver == false){
+        playerPlayed = this.value;
+        computerPlay();
+        if (
+            (playerPlayed === "Rock" && computerPlayed === "Scissors") ||
+            (playerPlayed === "Paper" && computerPlayed === "Rock") ||
+            (playerPlayed === "Scissors" && computerPlayed === "Paper")
+            ) {
+            playerScore++;
+            updateScore();
+        }
+        else if (
+            (playerPlayed === "Rock" && computerPlayed === "Paper") ||
+            (playerPlayed === "Paper" && computerPlayed === "Scissors") ||
+            (playerPlayed === "Scissors" && computerPlayed === "Rock")
+            ) {
+            computerScore++;
+            updateScore();
+        }
+        else {
+            updateScore();
         }
     }
 }
 
+function isGameActive(){
+    if(playerScore < 5 && computerScore < 5){
+
+        return true;
+    }
+    else {
+        document.getElementById('score').innerHTML = `GAME OVER! Player: ${playerScore} - Computer: ${computerScore} ... press New Game to play again`;
+        return false;
+    }
+}
+
 function computerPlay(){
-    let rnd = Math.floor(Math.random()*3)+1;
-    switch(rnd){
-        case 1:
-            computerPlayed = "Rock";
-            console.log("Computer played " + computerPlayed);
-            break;
-        case 2:
-            computerPlayed = "Paper";
-            console.log("Computer played " + computerPlayed);
-            break;
-        case 3:
-            computerPlayed = "Scissors";
-            console.log("Computer played " + computerPlayed);
-            break;
-    }
+    let rnd = Math.floor(Math.random()*3);
+    computerPlayed = choices[rnd];
 }
-/* Its not the smartest to make three separate functions, as this solution is not scalable, but whatever, its just a test without UI */
-function rock(){
-    playerPlayed = "Rock";
-    console.log("Player played " + playerPlayed);
-    computerPlay();
-    switch(computerPlayed){
-        case "Rock":
-            console.log("Its a Tie")
-            break;
-        case "Paper":
-            console.log("Paper beats rock, you lose")
-            computerScore++;
-            break;
-        case "Scissors":
-            console.log("Rock beats scissors, you win")
-            playerScore++;  
-            break;
+
+function updateScore(){
+    if(playerScore < 5 && computerScore < 5){
+        document.getElementById('score').innerHTML = `Player score: ${playerScore} ... Computer score: ${computerScore}`;
     }
-    console.log(`Player score: ${playerScore} ... Computer score: ${computerScore}`);
-}
-function paper(){
-    playerPlayed = "Paper";
-    console.log("Player played " + playerPlayed);
-    computerPlay();
-    switch(computerPlayed){
-        case "Paper":
-            console.log("Its a Tie")
-            break;
-        case "Scissors":
-            console.log("Scissors beat paper, you lose")
-            computerScore++;
-            break;
-        case "Rock":
-            console.log("Paper beats rock, you win")
-            playerScore++;
-            break;
+    else {
+        document.getElementById('score').innerHTML = `GAME OVER! Player: ${playerScore} - Computer: ${computerScore} ... press New Game to play again`;
+        gameOver = true;
     }
-    console.log(`Player score: ${playerScore} ... Computer score: ${computerScore}`);
-}
-function scissors(){
-    playerPlayed = "Scissors";
-    console.log("Player played " + playerPlayed);
-    computerPlay();
-    switch(computerPlayed){
-        case "Scissors":
-            console.log("Its a Tie")
-            break;
-        case "Rock":
-            console.log("Rock beats scissors, you lose")
-            computerScore++;
-            break;
-        case "Paper":
-            console.log("Scissors beat paper, you win")
-            playerScore++;
-            break;
-    }
-    console.log(`Player score: ${playerScore} ... Computer score: ${computerScore}`);
-}
+}let nodes = document.querySelectorAll('#Rock, #Paper, #Scissors');
+console.log(nodes);
+console.log(`ID ${nodes[0].id}`);
+console.log(nodes[0].classList[0]);
